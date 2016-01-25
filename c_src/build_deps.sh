c5d75d83ad70f9238b2b0407a365547f7313c8a9
Ty <lastcanal@gmail.com>
2016-01-25 16:37:52 -0500

    build libbitcoin from source

---
+++ c_src/build_deps.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+test `basename $PWD` != "c_src" && cd c_src
+
+dir=`pwd`
+
+case "$1" in
+  clean)
+    rm -rf install.sh .libs .deps
+    ;;
+
+  *)
+  	test -d .libs && exit 0
+    (test -f  install.sh || wget https://raw.githubusercontent.com/libbitcoin/libbitcoin/master/install.sh)
+    bash install.sh --disable-shared --with-tests=no --with-examples=no \
+                    --prefix=$dir/.libs --build-dir=$dir/.deps
+    ;;
+esac
