45e78031299217b5380f07d24665402fe73bf518
Ty <lastcanal@gmail.com>
2016-01-27 13:49:38 -0500

    copy libbitcoin and secp256k1 static objects into priv dir

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -8,13 +8,18 @@ dir=`pwd`
 
 case "$1" in
   clean)
-    rm -rf install.sh .libs .deps
+    rm -rf install.sh \
+           $dir/.libs \
+           $dir/.deps \
+           $dir/../priv/*.{so,a,la}
     ;;
 
   *)
-  	test -d .libs && exit 0
+    test -f ../priv/libbitcoin.la && exit 0
     (test -f  install.sh || wget https://raw.githubusercontent.com/libbitcoin/libbitcoin/master/install.sh)
     bash install.sh --disable-shared --with-tests=no --with-examples=no \
-                    --build-boost --build-icu --prefix=$dir/.libs --build-dir=$dir/.deps
+                    --prefix=$dir/.libs --build-dir=$dir/.deps
+
+    cp $dir/.libs/lib/*.{a,la} ../priv/
     ;;
 esac
