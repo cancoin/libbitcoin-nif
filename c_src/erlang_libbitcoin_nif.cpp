cf8ec54b75e665b863389ac9ac8e4bbce50f6462
Ty <lastcanal@gmail.com>
2016-10-07 12:13:38 -0400

    upgrade libbitcoin and use properties instead of fields

--- c_src/erlang_libbitcoin_nif.cpp
+++ c_src/erlang_libbitcoin_nif.cpp
@@ -54,38 +54,38 @@ erlang_libbitcoin_tx_decode(ErlNifEnv* env, int, const ERL_NIF_TERM argv[])
     std::map<nifpp::str_atom, nifpp::TERM> map_tx;
     long serialized_size = tx.serialized_size();
     ErlNifUInt64 total_output_value = tx.total_output_value();
-    map_tx["version"] = nifpp::make(env, tx.version);
-    map_tx["locktime"] = nifpp::make(env, tx.locktime);
+    map_tx["version"] = nifpp::make(env, tx.version());
+    map_tx["locktime"] = nifpp::make(env, tx.locktime());
     map_tx["hash"] = make_binary(env, hash256(tx.hash()).to_string());
     map_tx["coinbase"] = nifpp::make(env, tx.is_coinbase());
     map_tx["size"] = nifpp::make(env, serialized_size);
     map_tx["value"] = nifpp::make(env, total_output_value);
     std::list<nifpp::TERM> inputs;
-    for (const auto input: tx.inputs) {
+    for (const auto input: tx.inputs()) {
         std::map<nifpp::str_atom, nifpp::TERM> input_map;
         std::map<nifpp::str_atom, nifpp::TERM> prev_input_map;
-        prev_input_map["hash"] = make_binary(env, hash256(input.previous_output.hash).to_string());
-        prev_input_map["index"] = nifpp::make(env, input.previous_output.index);
+        prev_input_map["hash"] = make_binary(env, hash256(input.previous_output().hash()).to_string());
+        prev_input_map["index"] = nifpp::make(env, input.previous_output().index());
         input_map["previous_output"] = nifpp::make(env, prev_input_map);
-        const auto script_address = payment_address::extract(input.script, payment_address::mainnet_p2kh, payment_address::mainnet_p2sh);
+        const auto script_address = payment_address::extract(input.script(), payment_address::mainnet_p2kh, payment_address::mainnet_p2sh);
         if (script_address)
             input_map["address"] = make_binary(env, script_address.encoded());
-        input_map["script_asm"] = make_binary(env, chain::script(input.script).to_string(flags));
-        input_map["script"] = make_binary(env, encode_base16(chain::script(input.script).to_data(false)));
-        input_map["sequence"] = nifpp::make(env, input.sequence);
+        input_map["script_asm"] = make_binary(env, chain::script(input.script()).to_string(flags));
+        input_map["script"] = make_binary(env, encode_base16(chain::script(input.script()).to_data(false)));
+        input_map["sequence"] = nifpp::make(env, input.sequence());
         inputs.push_back(nifpp::make(env, input_map));
     }
     map_tx["inputs"] = nifpp::make(env, inputs);
     std::list<nifpp::TERM> outputs;
-    for (const auto output: tx.outputs) {
+    for (const auto output: tx.outputs()) {
         std::map<nifpp::str_atom, nifpp::TERM> output_map;
-        const auto script_address = payment_address::extract(output.script, payment_address::mainnet_p2kh, payment_address::mainnet_p2sh);
-        ErlNifUInt64 output_value = output.value;
+        const auto script_address = payment_address::extract(output.script(), payment_address::mainnet_p2kh, payment_address::mainnet_p2sh);
+        ErlNifUInt64 output_value = output.value();
         ErlNifUInt64 output_serialized_size = output.serialized_size();
         if (script_address)
             output_map["address"] = make_binary(env, script_address.encoded());
-        output_map["script_asm"] = make_binary(env, output.script.to_string(flags));
-        output_map["script"] = make_binary(env, encode_base16(chain::script(output.script).to_data(false)));
+        output_map["script_asm"] = make_binary(env, output.script().to_string(flags));
+        output_map["script"] = make_binary(env, encode_base16(chain::script(output.script()).to_data(false)));
         output_map["value"] = nifpp::make(env, output_value);
         output_map["size"] = nifpp::make(env, output_serialized_size);
         outputs.push_back(nifpp::make(env, output_map));
@@ -138,10 +138,10 @@ erlang_libbitcoin_tx_encode(ErlNifEnv* env, int, const ERL_NIF_TERM argv[])
       if (!nifpp::get(env, input_map["index"], index))
           return enif_make_badarg(env);
 
-      input.sequence = max_input_sequence;
-      std::copy(hash.data, hash.data + hash.size, point.hash.begin());
-      point.index = boost::lexical_cast<uint32_t>(index);
-      input.previous_output = point;
+      input.set_sequence(max_input_sequence);
+      std::copy(hash.data, hash.data + hash.size, point.hash().begin());
+      point.set_index(boost::lexical_cast<uint32_t>(index));
+      input.set_previous_output(point);
       inputs.push_back(input);
     }
 
@@ -230,13 +230,13 @@ erlang_libbitcoin_tx_encode(ErlNifEnv* env, int, const ERL_NIF_TERM argv[])
 
     chain::transaction tx;
 
-    tx.version = tx_version;
-    tx.locktime = locktime;
+    tx.set_version(tx_version);
+    tx.set_locktime(locktime);
 
     for (const chain::input& input: inputs)
-        tx.inputs.push_back(input);
+        tx.inputs().push_back(input);
     for (const chain::output& output: outputs)
-        tx.outputs.push_back(output);
+        tx.outputs().push_back(output);
 
     if (tx.is_locktime_conflict()) {
         return enif_make_badarg(env);
@@ -257,16 +257,16 @@ erlang_libbitcoin_header_decode(ErlNifEnv* env, int, const ERL_NIF_TERM argv[])
     chain::header header;
     header.from_data(make_data_chunk(env, &bin), false);
     auto hash = encode_hash(header.hash());
-    auto previous_block_hash = encode_hash(header.previous_block_hash);
+    auto previous_block_hash = encode_hash(header.previous_block_hash());
     std::map<nifpp::str_atom, nifpp::TERM> map_header;
     long serialized_size = header.serialized_size();
     map_header["size"] = nifpp::make(env, serialized_size);
-    map_header["version"] = nifpp::make(env, header.version);
+    map_header["version"] = nifpp::make(env, header.version());
     map_header["hash"] = make_binary(env, hash);
-    map_header["merkle"] = make_binary(env, encode_hash(header.merkle));
-    map_header["timestamp"] = nifpp::make(env, header.timestamp);
-    map_header["bits"] = nifpp::make(env, header.bits);
-    map_header["nonce"] = nifpp::make(env, header.nonce);
+    map_header["merkle"] = make_binary(env, encode_hash(header.merkle()));
+    map_header["timestamp"] = nifpp::make(env, header.timestamp());
+    map_header["bits"] = nifpp::make(env, header.bits());
+    map_header["nonce"] = nifpp::make(env, header.nonce());
     map_header["previous_block_hash"] = make_binary(env, previous_block_hash);
     nifpp::TERM term = nifpp::make(env, map_header);
     return term;
